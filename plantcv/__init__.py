a980298447dffce951c326dc0d2845af57eb4f10
Stephan Summerer <ssummerer@agrobios.it>
2016-11-18 12:13:24 +0100

    fixed implict relative references in __init__.py, added constants and new function

--- plantcv/__init__.py
+++ plantcv/__init__.py
@@ -1,6 +1,6 @@
-__all__ = ['fatal_error', 'constants', 'print_image', 'plot_image', 'plot_colorbar', 'readimage', 'laplace_filter','sobel_filter',
+__all__ = ['fatal_error', 'PCVconstants', 'print_image', 'plot_image', 'plot_colorbar', 'readimage', 'laplace_filter','sobel_filter',
            'scharr_filter', 'HistEqualization', 'plot_hist', 'image_add', 'image_subtract', 'erode', 'dilate',
-           'watershed', 'rectangle_mask', 'border_mask', 'rgb2gray_hsv', 'rgb2gray_lab', 'rgb2gray', 'binary_threshold',
+           'watershed', 'rectangle_mask', 'border_mask', 'rgb2gray_hsv', 'rgb2gray_lab', 'rgb2gray_rgb', 'rgb2gray', 'binary_threshold',
            'median_blur', 'fill', 'invert', 'logical_and', 'logical_or', 'logical_xor', 'apply_mask', 'find_objects',
            'define_roi', 'roi_objects', 'object_composition', 'analyze_object', 'analyze_bound', 'analyze_color',
            '_pseudocolored_image', 'analyze_NIR_intensity', 'fluor_fvfm', 'print_results', 'resize', 'flip',
@@ -9,66 +9,67 @@ __all__ = ['fatal_error', 'constants', 'print_image', 'plot_image', 'plot_colorb
            'x_axis_pseudolandmarks', 'y_axis_pseudolandmarks','gaussian_blur', 'cluster_contours','cluster_contour_splitimg',
            'rotate_img', 'shift_img']
 # MOST MODULES ARE DEPENDANT ON fata_error.py
-from fatal_error import fatal_error
+from .fatal_error import fatal_error
 
-import constants
+from .PCVconstants import *
 
-from print_image import print_image
-from plot_image import plot_image
-from plot_colorbar import plot_colorbar
-from readimage import readimage
-from laplace_filter import laplace_filter
-from sobel_filter import sobel_filter
-from scharr_filter import scharr_filter
-from HistEqualization import HistEqualization
-from plot_hist import plot_hist
-from image_add import image_add
-from image_subtract import image_subtract
-from erode import erode
-from dilate import dilate
-from watershed import watershed
-from rectangle_mask import rectangle_mask
-from border_mask import border_mask
-from rgb2gray_hsv import rgb2gray_hsv
-from rgb2gray_lab import rgb2gray_lab
-from rgb2gray import rgb2gray
-from binary_threshold import binary_threshold
-from median_blur import median_blur
-from fill import fill
-from invert import invert
-from logical_and import logical_and
-from logical_or import logical_or
-from logical_xor import logical_xor
-from apply_mask import apply_mask
-from find_objects import find_objects
-from define_roi import define_roi
-from roi_objects import roi_objects
-from object_composition import object_composition
-from analyze_object import analyze_object
-from analyze_bound import analyze_bound
-from analyze_color import analyze_color
-from analyze_NIR_intensity import analyze_NIR_intensity
-from fluor_fvfm import fluor_fvfm
-from print_results import print_results
-from resize import resize
-from flip import flip
-from crop_position_mask import crop_position_mask
-from get_nir import get_nir
-from adaptive_threshold import adaptive_threshold
-from otsu_auto_threshold import otsu_auto_threshold
-from report_size_marker_area import report_size_marker_area
-from white_balance import white_balance
-from white_balance2 import white_balance2
-from triangle_auto_threshold import triangle_auto_threshold
-from acute_vertex import acute_vertex
-from scale_features import scale_features
-from turgor_proxy import turgor_proxy
-from x_axis_pseudolandmarks import x_axis_pseudolandmarks
-from y_axis_pseudolandmarks import y_axis_pseudolandmarks
-from gaussian_blur import gaussian_blur
-from cluster_contours import cluster_contours
-from cluster_contour_splitimg import cluster_contour_splitimg
-from rotate_img import rotate_img
-from shift_img import shift_img
+from .print_image import print_image
+from .plot_image import plot_image
+from .plot_colorbar import plot_colorbar
+from .readimage import readimage
+from .laplace_filter import laplace_filter
+from .sobel_filter import sobel_filter
+from .scharr_filter import scharr_filter
+from .HistEqualization import HistEqualization
+from .plot_hist import plot_hist
+from .image_add import image_add
+from .image_subtract import image_subtract
+from .erode import erode
+from .dilate import dilate
+from .watershed import watershed
+from .rectangle_mask import rectangle_mask
+from .border_mask import border_mask
+from .rgb2gray_hsv import rgb2gray_hsv
+from .rgb2gray_lab import rgb2gray_lab
+from .rgb2gray_rgb import rgb2gray_rgb
+from .rgb2gray import rgb2gray
+from .binary_threshold import binary_threshold
+from .median_blur import median_blur
+from .fill import fill
+from .invert import invert
+from .logical_and import logical_and
+from .logical_or import logical_or
+from .logical_xor import logical_xor
+from .apply_mask import apply_mask
+from .find_objects import find_objects
+from .define_roi import define_roi
+from .roi_objects import roi_objects
+from .object_composition import object_composition
+from .analyze_object import analyze_object
+from .analyze_bound import analyze_bound
+from .analyze_color import analyze_color
+from .analyze_NIR_intensity import analyze_NIR_intensity
+from .fluor_fvfm import fluor_fvfm
+from .print_results import print_results
+from .resize import resize
+from .flip import flip
+from .crop_position_mask import crop_position_mask
+from .get_nir import get_nir
+from .adaptive_threshold import adaptive_threshold
+from .otsu_auto_threshold import otsu_auto_threshold
+from .report_size_marker_area import report_size_marker_area
+from .white_balance import white_balance
+from .white_balance2 import white_balance2
+from .triangle_auto_threshold import triangle_auto_threshold
+from .acute_vertex import acute_vertex
+from .scale_features import scale_features
+from .turgor_proxy import turgor_proxy
+from .x_axis_pseudolandmarks import x_axis_pseudolandmarks
+from .y_axis_pseudolandmarks import y_axis_pseudolandmarks
+from .gaussian_blur import gaussian_blur
+from .cluster_contours import cluster_contours
+from .cluster_contour_splitimg import cluster_contour_splitimg
+from .rotate_img import rotate_img
+from .shift_img import shift_img
 
 #add new functions to end of lists
