e1f118ce1348bef97b7c0b6b642d4290436cbc99
Stephan Summerer <ssummerer@agrobios.it>
2017-05-17 09:44:00 +0200

    Danforth modification to make error function for not reading an image

--- plantcv/readimage.py
+++ plantcv/readimage.py
@@ -3,34 +3,40 @@
 import os
 import cv2
 from . import fatal_error
+from . import print_image
+from . import plot_image
 #opencv2 version control
 ( cv2major, cv2minor, _) = cv2.__version__.split('.')
 ( cv2major, cv2minor) = int( cv2major), int( cv2minor)
 
 
-def readimage(filename):
+def readimage(filename, debug=None):
     """Read image from file.
-
     Inputs:
     filename = name of image file
-
+    debug    = None, print, or plot. Print = save to file, Plot = print to screen.
     Returns:
     img      = image object as numpy array
     path     = path to image file
     img_name = name of image file
-
     :param filename: str
+    :param debug: str
     :return img: numpy array
     :return path: str
     :return img_name: str
     """
 
-    try:
-        img = cv2.imread( filename)
-    except:
-        fatal_error( "Cannot open " + filename)
+    img = cv2.imread(filename)
+
+    if img is None:
+        fatal_error("Failed to open " + filename)
 
     # Split path from filename
-    path, img_name = os.path.split( filename)
+    path, img_name = os.path.split(filename)
+
+    if debug == "print":
+        print_image(img, "input_image.png")
+    elif debug == "plot":
+        plot_image(img)
 
     return img, path, img_name
